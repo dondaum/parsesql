bcaca7d339c72bc1b8867fff11a74d5400211301
Daum <S40844@u-ssi.net>
2019-11-08 17:19:32 +0100

    refactored executer and add a sequential executer

--- parser/app.py
+++ parser/app.py
@@ -2,19 +2,17 @@ from main.sql_parser.snowsqlparser import ParseSql
 from main.sql_parser.file_finder import FileFinder
 from main.database.db_engine import Session
 from main.database.models import TableDependency
+from main.executers import SequentialExecuter, MultiProcessingExecuter
 import uuid
-#import multiprocessing
-from multiprocessing import Pool, cpu_count, Queue
 import cProfile
 import time
 
 
 class Runner(object):
-    def __init__(self, parallelism=1):
+    def __init__(self, parallelism=0):
         self.allfiles = None
         self.dependencies = list()
         self.parallelism = parallelism
-        self.proccess = self.determine_max_proc()
         self.executer = self.get_executer()
 
     def get_executer(self):
@@ -23,36 +21,13 @@ class Runner(object):
         else:
             return MultiProcessingExecuter()
 
-    def dryrun(self):
+    def parseSql(self):
         self.executer.to_parse_files = self.allfiles
         result = self.executer.run()
         self.dependencies = result
      
-    def findFiles(self):
+    def findFiles(self):    
         self.allfiles = FileFinder().getListOfFiles()
-    
-    def parseSql(self):
-        for file in self.allfiles:
-            self.dependencies.append(ParseSql(file=file).getfinalFrom())
-    
-    def exp_para_parseSql(self, file):
-        self.dependencies.append(ParseSql(file=file).getfinalFrom())
-
-    def para_parseSql(self):
-        number_of_processcess = self.determine_max_proc() - 1
-        print(f"starting with number of processes: {number_of_processcess} ")
-        # p = Pool(number_of_processcess)
-        # p.map(self.exp_para_parseSql, self.allfiles )
-
-        with Pool(processes=number_of_processcess) as pool:
-            data = pool.map(self.exp_para_parseSql, self.allfiles)
-            self.dependencies = data
-            print('-------------------')
-            print(f'Typ {type(data)}')
-            print(data)
-    
-    def determine_max_proc(self):
-        return cpu_count()
 
     def unitendtest(self):
         #unintend
@@ -110,34 +85,12 @@ class Runner(object):
         self.findFiles()
 
         #self.allfiles = self.findFiles()
-        self.dryrun()
-        #self.parseSql()
+
+        self.parseSql()
         #self.para_parseSql()
         #self.insertdep()
-        self.bulkinsertdep()
+        #self.bulkinsertdep()
 
-class BaseExecuter(object):
-    pass
-
-class SequentialExecuter(BaseExecuter):
-    pass
-
-class MultiProcessingExecuter(BaseExecuter):
-
-    def __init__(self, to_parse_files=None):
-        self.to_parse_files = to_parse_files
-
-    def determine_max_proc(self):
-        return cpu_count()
-
-    def parse(self, file):
-        return ParseSql(file=file).getfinalFrom()
-
-    def run(self):
-        number_of_processcess = self.determine_max_proc() - 1
-
-        p = Pool(number_of_processcess)
-        return p.map( self.parse , self.to_parse_files )
 
 if __name__ == "__main__":
     starttime = time.time()
