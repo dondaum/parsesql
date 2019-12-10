4f3a9d2e0b33b387b3e2b09ec659d37caaa54600
Sebastian <sebastian.daum89@gmail.com>
2019-12-10 08:39:33 +0100

    Update sqlite_to_csv.py
    Add licence and TODO comments

--- parser/util/sqlite_to_csv.py
+++ parser/util/sqlite_to_csv.py
@@ -1,3 +1,25 @@
+# MIT License
+
+# Copyright (c) 2019 Sebastian Daum
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import os, csv
 from sqlalchemy import create_engine, exc, MetaData, Table
 from sqlalchemy.sql import select
@@ -25,11 +47,12 @@ def split_row(rowproxy):
         row=row_as_list
         splittable=False
     return row, splittable
-
+#TODO: Check if this path works system independent
 engine = create_engine('sqlite:///{0}'.format(sqlite_db_file))
 
 connection = engine.connect()
 
+#TODO: This should be included from the existing sqlalchemy metdata
 metadata = MetaData()
 tabledependencies = Table('tabledependencies', metadata, autoload=True, autoload_with=engine)
 
