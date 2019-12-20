41f1604b41a49bbe86b88d011d58104c61f4df18
Sebastian Daum <sebastian.daum89@gmail.com>
2019-12-20 19:41:44 +0100

    set up basic test directory structure

---
+++ parser/tests/test_configuration.py
@@ -0,0 +1,31 @@
+import unittest
+from config import config_reader
+from pathlib import Path
+
+
+class ConfigurationTest(unittest.TestCase):
+
+    def test_if_missing_config_raise_error(self):
+        """
+        test if wrong file name will exit programm
+        """
+        with self.assertRaises(SystemExit):
+            config_reader.Configuration(filename='ABS')
+
+    def test_if_config_class_exists(self):
+        """
+        test if configuration class is available 
+        """
+        klass = config_reader.Configuration(filename='example_configuration.json')
+        self.assertEqual(klass.__class__.__name__, "Configuration")
+
+    def test_if_path_object_gets_created(self):
+        """
+        test if config object is an instance of Path class
+        """
+        c = config_reader.Configuration(filename='example_configuration.json')
+        self.assertIsInstance(c.get_sql_directory(), Path)
+
+
+if __name__ == "__main__":
+    unittest.main()
